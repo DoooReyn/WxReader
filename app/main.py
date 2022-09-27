b60c88f20b462a819c6d083b4fa64104a7f95285
reyn_gitee <964416269@qq.com>
2022-09-27 11:26:09 +0800

    第一个提交

---
+++ app/main.py
@@ -0,0 +1,11 @@
+#
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
