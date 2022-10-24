3c71df5bb242748448e5d6e68757dfaa1f897132
DoooReyn <jl88744653@gmail.com>
2022-10-24 13:32:54 +0800

    修复调试模式和UPX环境变量问题

--- scripts/package/cmm.py
+++ scripts/package/cmm.py
@@ -3,6 +3,7 @@ from os import environ
 
 
 class Cache:
+    """缓存"""
     def __init__(self):
         self.debug = False
         self.upx = False
@@ -12,29 +13,36 @@ class Cache:
         self.env = environ
 
     def setVersion(self, version: [str]):
+        """设置版本号"""
         self.display_version = '.'.join(version[:3])
         self.production_version = '.'.join(version)
 
     def setDebug(self, debug: bool):
+        """设置调试模式"""
         self.debug = debug
-        self.setEnv('CEFPYTHON_PYINSTALLER_DEBUG', '1')
+        if debug:
+            self.setEnv('CEFPYTHON_PYINSTALLER_DEBUG', '1')
 
     def setUpx(self, upx: bool):
+        """设置是否使用upx压缩"""
         self.upx = upx
-        self.setEnv('CEFPYTHON_PYINSTALLER_UPX', '1')
+        if upx:
+            self.setEnv('CEFPYTHON_PYINSTALLER_UPX', '1')
 
     def setManifest(self, manifest):
+        """设置项目配置"""
         self.manifest = manifest
 
     @staticmethod
     def getEnv(key, default=None):
+        """获取环境变量"""
         return environ.get(key, default)
 
     @staticmethod
     def setEnv(key, value):
+        """设置环境变量"""
         environ[key] = value
 
 
 cache = Cache()
-
 logger = log.getLogger('Bundle')
